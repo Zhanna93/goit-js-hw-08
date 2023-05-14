e130ecdc5591278e62111b27cb1f52e2dbe46af6
Zhanna93 <121199582+Zhanna93@users.noreply.github.com>
2023-05-14 20:17:28 +0000

    Deploying to gh-pages from @ Zhanna93/goit-js-hw-08@f32ec886131a85f7a2af23abaed7e491b90d4708 🚀

---
+++ 03-feedback.fa2a8a48.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("form");console.log(e),e.addEventListener("input",(({target:{value:e}})=>{localStorage.setItem("feedback-form-state",JSON.stringify({email:e,message:e}))}));
+//# sourceMappingURL=03-feedback.fa2a8a48.js.map
