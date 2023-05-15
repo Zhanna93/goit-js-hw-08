ec1e829658e86192c674de5359a633dbdfbd9702
Zhanna93 <121199582+Zhanna93@users.noreply.github.com>
2023-05-15 11:40:24 +0000

    Deploying to gh-pages from @ Zhanna93/goit-js-hw-08@f25b9dd2f8e32090b61a76f24df1be60af1faa49 🚀

--- 03-feedback.ae2a3f77.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("form");console.log(e),e.addEventListener("input",(function(e){var t=e.target.value;localStorage.setItem("feedback-form-state",JSON.stringify({email:t,message:t}))}))}();
-//# sourceMappingURL=03-feedback.ae2a3f77.js.map
--- 03-feedback.fa2a8a48.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form");console.log(e),e.addEventListener("input",(({target:{value:e}})=>{localStorage.setItem("feedback-form-state",JSON.stringify({email:e,message:e}))}));
-//# sourceMappingURL=03-feedback.fa2a8a48.js.map
